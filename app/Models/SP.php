9add340d2ec894645321a2c82c56042b49972107
abdallah991 <alathamneh.abdallah@gmail.com>
2023-11-26 09:42:35 +0300

    Sp model fix

---
+++ app/Models/SP.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SP extends Model
+{
+    use HasFactory;
+
+    // table name
+    protected $table = 'sp';
+    // fillable items in the create method
+    protected $fillable = [
+        'platformId', 'firstName', 'lastName', 'email', 'phone', 'xp', 'level', 'gender', 'nationality', 'profilePicture',
+        'cprPicture', 'pictureChanged', 'dob', 'acadamicQualification', 'acadamicSpecialization', 'employment', 'howDidYouHear',
+        'sp', 'progresses', 'registrations'
+    ];
+
+    protected $casts = [
+        'dob' => 'date',
+    ];
+}
